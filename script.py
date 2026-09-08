d0529593229178bd31eb855349fc0ca624b0acfb
Kravchuk-Roman21KI <romakrauchuk@gmail.com>
2026-09-08 23:47:34 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,41 @@
+print('Hello, World')
+
+
+
+
+
+a = 12
+b = 4
+print('Арифметичні оператори:')
+print('Додавання: ', a + b)
+print('Віднімання: ', a - b)
+print('Множення: ', a * b)
+print('Ділення: ', a / b)
+print('Цілочисельне ділення: ', a // b)
+print('Остача від ділення: ', a % b)
+print('Піднесення до степення: ', a ** b)
+
+
+
+
+
+
+
+age = 16
+h = 5
+name = 'Roma'
+n = True
+list = [1, 2, 3, 4, 5]
+tool = (1, 2, 3, 4, 5, 6)
+dict = {10: '1',
+        11: '2'}
+set = {1, 2, 3, 4, 5}
+print(age, name)
+print(age, type(age))
+print(h, type(h))
+print(name, type(name))
+print(n, type(n))
+print(list, type(list))
+print(tool, type(tool))
+print(dict, type(dict))
+print(set, type(set))
